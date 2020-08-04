098bfe9e643f178b98daf9be0e2f919e951aa3b5
clarisson28 <reddieson05@gmail.com>
2020-08-04 15:54:26 +0800

    Variables and Data Types

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-print("   /|")
-print("  / |")
-print(" /  |")
-print("/___|")
+character_name = "John"
+character_age = "35"
 
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old.")
+print("He really liked his name " + character_name + ",")
+print("but didn't like being " + character_age + ".")
