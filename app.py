76fff2a77a21ef60d7915d33f718a1e160271585
clarisson28 <reddieson05@gmail.com>
2020-08-06 21:53:37 +0800

    Working with Strings

--- app.py
+++ app.py
@@ -1,7 +1,57 @@
-character_name = "John"
-character_age = "35"
+# WORKING WITH STRINGS
 
-print("There once was a man named " + character_name + ",")
-print("he was " + character_age + " years old.")
-print("He really liked his name " + character_name + ",")
-print("but didn't like being " + character_age + ".")
+# Printing a string
+# print("Giraffe Academy")
+
+# Create a new line in string
+# print("Giraffe\nAcademy")
+
+# Putting a quotation mark inside a string
+# print("Giraffe\"Academy")
+
+# String variable
+# phrase = "Giraffe Academy"
+# print(phrase)
+
+# Concatenation
+# phrase = "Giraffe Academy"
+# print(phrase + " is awesome")
+
+# Functions in string:
+
+# ~Turn the entire string to lower case
+# phrase = "Giraffe Academy"
+# print(phrase.lower())
+
+# ~Turn the entire string to upper case
+# phrase = "Giraffe Academy"
+# print(phrase.upper())
+
+# ~Check if the entire string is in upper case
+# phrase = "Giraffe Academy"
+# print(phrase.isupper())
+
+# ~Turn the entire string to upper case then check if the entire string is in upper case
+# phrase = "Giraffe Academy"
+# print(phrase.upper().isupper())
+
+# ~Figure out how many characters are inside the string
+# phrase = "Giraffe Academy"
+# print(len(phrase))
+
+# ~Grab an individual character in a string
+# phrase = "Giraffe Academy"
+#           0123456789.....
+# print(phrase[0])
+# print(phrase[3])
+
+# ~Tell where a specific character is located in a string
+# phrase = "Giraffe Academy"
+# print(phrase.index("G"))
+# print(phrase.index("a"))
+# print(phrase.index("Acad"))
+# print(phrase.index("z"))
+
+# ~Replace function
+# phrase = "Giraffe Academy"
+# print(phrase.replace("Giraffe", "Elephant"))
